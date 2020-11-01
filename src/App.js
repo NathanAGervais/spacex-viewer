732704324bde0b9a15b2c5ce0d5b95d301cb791c
NG <nathan.gervais@connect.auto>
2020-11-01 20:22:23 +0000

    adds launch page and very rough api reducer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './theme/theme';
 import GlobalStyle from './theme/GlobalStyle';
 import { Header } from './components/Header';
+import { Launches } from './components/Launches';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header />
+      <Launches />
     </ThemeProvider>
   );
 }
