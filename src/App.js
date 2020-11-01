487593b357e6a01d8aa8d81fac408f609177cf7d
NG <nathan.gervais@connect.auto>
2020-11-01 20:19:57 +0000

    removes theme provider to app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme/theme';
+import GlobalStyle from './theme/GlobalStyle';
 
 function App() {
   return <div className="App">Hello World</div>;
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
 }
 
 export default App;
